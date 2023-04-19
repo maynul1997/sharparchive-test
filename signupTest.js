38af33f64ec099eb815baa9d03e8f8db6ef37abf
maynul <mamun.harf@gmail.com>
2023-04-19 12:56:16 +0600

    inputName

--- signupTest.js
+++ signupTest.js
@@ -7,7 +7,7 @@ test('Valid Signup test', async t=>{
     await t
     .setNativeDialogHandler(() => true)
     .click(Selector("span").withText("Sign Up"));
-    await t.debug();
+    
     await t.typeText("#first_name", "Md");
 
 })
